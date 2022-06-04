0985f9e1fed04fecab3313356afb87fce87e30e2
ziippy <ziippy@naver.com>
2022-06-04 20:11:00 +0900

    chapter8 - 아이템55

---
+++ src/chapter8/item56/Item56.java
@@ -0,0 +1,31 @@
+package chapter8.item56;
+
+
+import java.util.Collection;
+import java.util.Optional;
+
+class Sample {
+    /**
+     * 지정한 위치의 원소를 반환한다.
+     *
+     * @param index 반환할 원소의 인덱스; 0 이상이고 10 이하여야 한다.
+     * @return 지정한 위치의 원소 (Sample 클래스 타입)
+     * @throws IndexOutOfBoundsException index 가 범위를 벗어나면,
+     *         즉, index < 0 또는 index > 10 이면 발생한다.
+     */
+    Sample get(int index) {
+        return new Sample();
+    }
+}
+
+public class Item56 {
+
+
+
+    public static void main(String[] args) {
+
+
+
+    }
+}
+
