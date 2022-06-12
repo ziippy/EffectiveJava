a56e59b2bc3d4e2b8a9915960eb50f9098aeb34c
ziippy <ziippy@naver.com>
2022-06-13 08:06:47 +0900

    chapter9 - 아이템65

--- src/chapter9/README.md
+++ src/chapter9/README.md
@@ -21,3 +21,5 @@
 #### < [아이템63](https://github.com/ziippy/EffectiveJava/tree/master/src/chapter9/item63) > 문자열 연결은 느리니 주의하라
 
 #### < [아이템64](https://github.com/ziippy/EffectiveJava/tree/master/src/chapter9/item64) > 객체는 인터페이스를 사용해 참조하라
+
+#### < [아이템65](https://github.com/ziippy/EffectiveJava/tree/master/src/chapter9/item65) > 리플렉션보다는 인터페이스를 사용하라
---
+++ src/chapter9/item65/Item65.java
@@ -0,0 +1,57 @@
+package chapter9.item65;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Arrays;
+import java.util.Set;
+
+
+public class Item65 {
+
+    public static void main(String[] args) {
+
+        // 클래스 이름을 Class 객체로 변환
+        Class<? extends Set<String>> cl = null;
+        try {
+            //cl = (Class<? extends Set<String>>)Class.forName(args[0]);
+            cl = (Class<? extends Set<String>>)Class.forName("java.lang.String");
+        }  catch (ClassNotFoundException e) {
+            fatalError("클래스를 찾을 수 없습니다.");
+        }
+
+        // 생성자를 얻는다.
+        Constructor<? extends Set<String>> cons = null;
+        try {
+            cons = cl.getDeclaredConstructor();
+        } catch (NoSuchMethodException e) {
+            fatalError("매개변수 없는 생성자를 찾을 수 없습니다.");
+        }
+
+        // 집합의 인스턴스를 만든다.
+        Set<String> s = null;
+        try {
+            s = cons.newInstance();
+        } catch (IllegalAccessException e) {
+            fatalError("생성자에 접근할 수 없습니다.");
+        } catch (InstantiationException e) {
+            fatalError("클래스를 인스턴스화 할 수 없습니다.");
+        } catch (InvocationTargetException e) {
+            fatalError("생성자가 예외를 던졌습니다. : " + e.getCause());
+        } catch (ClassCastException e) {
+            fatalError("Set을 구현하지 않은 클래스입니다.");
+        }
+
+        // 생성한 집합을 사용한다.
+        s.addAll(Arrays.asList("1", "2", "3"));
+        System.out.println(s);
+        /** 출력
+         * Set을 구현하지 않은 클래스입니다.
+         */
+    }
+
+    private static void fatalError(String msg) {
+        System.err.println(msg);
+        System.exit(1);
+    }
+}
+
